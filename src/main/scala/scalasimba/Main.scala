091d8db3c2b6dbf008ee288613fd24c1094e4534
Tanvir <tahmedcs17@gmail.com>
2018-06-11 15:16:58 +0200

    Reading CSV file and run range queries in SIMBA

--- src/main/scala/scalasimba/Main.scala
+++ src/main/scala/scalasimba/Main.scala
@@ -2,13 +2,20 @@ package main.scala.scalasimbas
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.simba.SimbaSession
+import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.types._
+
 
 object Main {
   case class PointData(x: Double, y: Double, z: Double, other: String)
 
+
+
   def main(args: Array[String]) {
-    println("Hello World")
-//
+
+
+     // println("Hello World")
+
     val simbaSession = SimbaSession
       .builder()
       .master("local[4]")
@@ -17,6 +24,8 @@ object Main {
       .getOrCreate()
 
     runRangeQuery(simbaSession)
+
+
     //Create a SparkContext to initialize Spark
     /* val conf = new SparkConf()
     conf.setMaster("local")
@@ -35,17 +44,77 @@ object Main {
     System.out.println("Total words: " + counts.count());
     */
     //counts.saveAsTextFile("/tmp/shakespeareWordCount")
+
   }
+
   private def runRangeQuery(simba: SimbaSession): Unit = {
 
     System.out.println("Running Range Queries...")
 
+
+
+     /*  Reading CSV file from external source
+     *   To do : Read CSV from HDFS source
+     */
+
+    // Defining Schema First Approach
+    /*
+    case class TripFareSchema(Medallion: String, HackLicense: String, VendorId: String,
+                              PickupDatetime : String, PaymentType: String, FareAmount: Double, SurCharge : Double, MtaTax:Double
+                             , TipAmount : Double , TollsAmount: Double , TotalAmount: Double )
+
+    var tripFareSchema = Encoders.product[TripFareSchema].schema
+   */
+
+    // Defining Schema Second Approach
+    var tripFareSchema = StructType(Array(
+
+      StructField("Medallion",    StringType, true),
+      StructField("HackLicense",  StringType, true),
+      StructField("VendorId",  StringType, true),
+      StructField("PickupDatetime",  StringType, true),
+      StructField("PaymentType",  StringType, true),
+      StructField("FareAmount",  DoubleType, true),
+      StructField("SurCharge",  DoubleType, true),
+      StructField("MtaTax",  DoubleType, true),
+      StructField("TipAmount",  DoubleType, true),
+      StructField("TollsAmount",  DoubleType, true),
+      StructField("TotalAmount",  DoubleType, true)
+
+    ))
+
+
+
+    val csVTripFareDS = simba.read
+      .format("csv")
+      .option("header", "true") //reading the headers
+      .option("mode", "DROPMALFORMED")
+      .schema(tripFareSchema)
+      .load("src/main/resources/trip_fare_10000.csv")
+
+    // To view schemas
+    csVTripFareDS.printSchema()
+
+    // To view full tables first 20 rows
+    csVTripFareDS.show()
+
+    // To view spcific columns
+    csVTripFareDS.select("FareAmount","TipAmount","VendorId","PickupDatetime").show()
+
+    /*
     import simba.implicits._
     val caseClassDS = Seq(PointData(1.0, 1.0, 3.0, "1"),  PointData(2.0, 2.0, 3.0, "2"), PointData(2.0, 2.0, 3.0, "3"),
       PointData(2.0, 2.0, 3.0, "4"),PointData(3.0, 3.0, 3.0, "5"),PointData(4.0, 4.0, 3.0, "6")).toDS()
+   */
+
+    //  val rangeTotalAmount = csVTripFareDS.select("Medallion")
 
     import simba.simbaImplicits._
-    caseClassDS.range(Array("x", "y"),Array(1.0, 1.0),Array(3.0, 3.0)).show(10)
+
+    // caseClassDS.range(Array("x", "y"),Array(1.0, 1.0),Array(3.0, 3.0)).show(10)
+
+    // Here the resultset will contain only those rows where  25.0 => FareAmount >= 0.0 ,  2.0 => TipAmount >= 0.0
+    csVTripFareDS.range(Array("FareAmount", "TipAmount"),Array(0.0, 0.0),Array(25.0, 2.0)).show(10)
 
   }
 
